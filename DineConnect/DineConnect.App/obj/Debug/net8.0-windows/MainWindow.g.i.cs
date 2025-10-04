e257e302e1214a185103be97187ae5bc197aa8f4
Hanna <Hanna@WIN-AGL6C6RN32U>
2025-10-04 18:40:40 +1000

    Add all project dependencies and nuget packages

--- DineConnect/DineConnect.App/obj/Debug/net8.0-windows/MainWindow.g.i.cs
+++ DineConnect/DineConnect.App/obj/Debug/net8.0-windows/MainWindow.g.i.cs
@@ -53,7 +53,7 @@ namespace DineConnect.App {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/DineConnect.App;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/DineConnect.App;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
